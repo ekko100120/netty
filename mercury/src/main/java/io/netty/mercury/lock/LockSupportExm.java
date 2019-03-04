338140d8d1bd9bd5cc8375c6752757dfc524bbbb
kenny <411316753@qq.com>
2019-03-04 19:43:20 +0800

    add some concurrency example

--- mercury/src/main/java/io/netty/mercury/lock/LockSupportExm.java
+++ mercury/src/main/java/io/netty/mercury/lock/LockSupportExm.java
@@ -10,42 +10,64 @@ public class LockSupportExm {
             public void run() {
                 Thread.currentThread().setName("t1");
                 System.out.println(Thread.currentThread().getName()+" before park");
-                LockSupport.parkNanos(TimeUnit.NANOSECONDS.convert(10,TimeUnit.SECONDS));
+                LockSupport.parkNanos(TimeUnit.NANOSECONDS.convert(20,TimeUnit.SECONDS));
+                System.out.println(Thread.currentThread().getName()+" after park");
+            }
+        };
+
+        final Thread t3 = new Thread(){
+            @Override
+            public void run() {
+                Thread.currentThread().setName("t3");
+                System.out.println(Thread.currentThread().getName()+" park 5s");
+                LockSupport.parkUntil(System.currentTimeMillis()+TimeUnit.MILLISECONDS.convert(8,TimeUnit.SECONDS));
                 System.out.println(Thread.currentThread().getName()+" after park");
             }
         };
         Thread t2 = new Thread(){
             @Override
             public void run() {
-                Thread.currentThread().setName("t2");
-                System.out.println(Thread.currentThread().getName()+" unpark t1");
                 try {
+                    Thread.currentThread().setName("t2");
                     TimeUnit.SECONDS.sleep(2);
+                    System.out.println(Thread.currentThread().getName()+" unpark t1");
+                    LockSupport.unpark(t3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 //                LockSupport.unpark(t1);
-                LockSupport.parkNanos(TimeUnit.NANOSECONDS.convert(10,TimeUnit.SECONDS));
+
             }
         };
-        Thread t3 = new Thread(){
+
+        Thread t4 = new Thread(){
             @Override
             public void run() {
-                Thread.currentThread().setName("t3");
-                System.out.println(Thread.currentThread().getName()+" park 5s");
-                LockSupport.parkUntil(System.currentTimeMillis()+TimeUnit.MICROSECONDS.convert(5,TimeUnit.SECONDS));
-                System.out.println(Thread.currentThread().getName()+"a fter park");
+                Thread.currentThread().setName("t4");
+                try {
+                    TimeUnit.SECONDS.sleep(6);
+                    System.out.println(Thread.currentThread().getName()+" unpark t3");
+                    LockSupport.unpark(t3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         };
         t1.start();
         t2.start();
         t3.start();
+//        t4.start();
         try {
             t1.join();
             t2.join();
             t3.join();
+//            t4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }finally {
+            System.out.println("main thread over----");
         }
 
     }
